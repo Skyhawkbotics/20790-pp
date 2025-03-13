03ee74ef690d83b7743a658f03fa9040dc75160a
Trevor Chung <trevor271c@gmail.com>
2025-03-13 07:26:45 -0700

    Converting the right auto to a left auto code

--- TeamCode/src/main/java/pedroPathing/autonomous/left_auto.java
+++ TeamCode/src/main/java/pedroPathing/autonomous/left_auto.java
@@ -31,9 +31,7 @@ import pedroPathing.constants.LConstants;
 
 @Config
 @Autonomous(name = "RIGHT AUTO", group = "AUTO")
-///* This is the Spark invitational auto
-/// We won spark 2nd in placement, 1st pick alliance, and also 2nd inspire.
-/// This  is a 4+0 autonomous and park for the into the deep season, using bezier curves and linear interpolation, along with a nano-second timer
+
 public class left_auto extends OpMode {
 
     private Follower follower;
@@ -43,31 +41,33 @@ public class left_auto extends OpMode {
     private NanoTimer pathTimer; // Timers for progression of states, in nano seconds
 
     // Important Pose parts
-    double pickup_x = 0.6; // Distance from the claw to the sample
+    /*double pickup_x = 0.6; // Distance from the claw to the sample
     double turn_distance_x = 20; // Distance to start pickup
-    double pickups_y = 41.00; // Observation Zone pickup distance
-    private int pathState, armState, outclawState, outgrabState; // Different cases and states of the different parts of the robot
+    double pickups_y = 41.00; // Observation Zone pickup distance*/
+
+    //Initializing the states for the robot
+    private int pathState, upArmState, outArmState, intakeState, outtakeState; // Different cases and states of the different parts of the robot
 
     // Poses, and pickUp Poses
     private Pose startPose = new Pose(10, 67.0, Math.toRadians(0)); //TODO
     private Pose bucketPose = new Pose(15, 129, Math.toRadians(135)); //TODO
 
     /// Paths, and path chains : pushFirst and pushSecond are called after hangFirst
-    private Path preload, hang1, pickup1, pickup2, pickup3, hang2, hang3, park;
-    private PathChain pushFirst, pushSecond;
+    private Path ; //TODO: Trevor
+    private PathChain ;//TODO: Trevor as well
 
-    // Motors
-    private DcMotorEx up, out; // Slide motors
-    private Servo servo_outtake_wrist, servo_intake_wrist;
-    private CRServo servo_outtake, servo_intake;
+    /// Motors
+    private Servo servo_outtake_flip1, servo_outtake_flip2, servo_intake_wrist, servo_intake_rotate, sweeper, servo_intake, servo_outtake, servo_outtake_rotate;
+    private DcMotorEx up1, up2, out;
+    private TouchSensor up_zero, out_zero;
     private TouchSensor up_zero;
     private Telemetry telemetryA;
 
-    // variables
-    int up_hanging_position = 1778; //DONE: calibrate this value, viper slide position to
-    int up_hanging_position_done = 1559; //TODO: calibrate this value, position of viper slide when releasing after speciman is on the bar.
+    /// variables
+    int up_basket_position = ; //TODO: calibrate this value, slide position to
 
-    public void buildPaths() {
+
+    public void buildPaths() { //TODO: Trevor needs to change these paths
                 // preload
         preload = new Path(
                 new BezierLine(
@@ -140,7 +140,7 @@ public class left_auto extends OpMode {
 
         }
     }
-    public void autonomousActionUpdate() {
+    /*public void autonomousActionUpdate() {
             switch (armState) {
                 case -1: // default stop
                     up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -204,6 +204,9 @@ public class left_auto extends OpMode {
                 }
                 break;
         }
+    }*/
+    public void autonomousActionUpdate() { //TODO: Ruben... Set up variables for all necessary values so it's easy to calibrate
+
     }
     public void setPathState(int pState) {
         pathState = pState;
@@ -270,10 +273,15 @@ public class left_auto extends OpMode {
         telemetryA.update();
 
         //setup arms init
-        up = hardwareMap.get(DcMotorEx.class, "up");
-        up.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        up.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        up.setDirection(DcMotorSimple.Direction.REVERSE);
+        up1 = hardwareMap.get(DcMotorEx.class, "up1"); //DONE: Is this still true?
+        up1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        up1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        up1.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        up2 = hardwareMap.get(DcMotorEx.class, "up2"); //DONE: Is this still true?
+        up2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        up2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        up2.setDirection(DcMotorSimple.Direction.REVERSE);
 
         out = hardwareMap.get(DcMotorEx.class, "out");
         int charles = out.getCurrentPosition();
@@ -282,14 +290,20 @@ public class left_auto extends OpMode {
         out.setPower(1);
 
         // Servos init
-        servo_outtake = hardwareMap.get(CRServo.class,"outtake");
-        servo_intake = hardwareMap.get(CRServo.class, "intake");
+        servo_intake = hardwareMap.get(Servo.class, "intake");
+        servo_intake_rotate = hardwareMap.get(Servo.class, "intakeRotate");
         servo_intake_wrist = hardwareMap.get(Servo.class, "intakeWrist");
-        servo_outtake_wrist = hardwareMap.get(Servo.class, "outtakeWrist");
-        servo_outtake_wrist.setPosition(0);
 
-        // Sensor init
+
+        servo_outtake_flip1 = hardwareMap.get(Servo.class, "ofip1");
+        servo_outtake_flip2 = hardwareMap.get(Servo.class, "ofip2");
+
+        servo_outtake = hardwareMap.get(Servo.class, "outtake");
+        servo_outtake_rotate = hardwareMap.get(Servo.class, "outtaker");
+
+        //Sensor init
         up_zero = hardwareMap.get(TouchSensor.class, "up_zero");
+
     }
 
     /** This method is called continuously after Init while waiting for "play", it's not necessary but its useful to give more time to build paths **/
@@ -308,6 +322,7 @@ public class left_auto extends OpMode {
     public void start() {
         buildPaths();
         opmodeTimer.resetTimer();
+        //TODO: Change all of these values below
         setPathState(2);
         setArmState(-1);
         setoutGrabState(0);
